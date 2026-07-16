30509cc4d776862108eff1656f04fd800b9b6de7
iremnergiz <iremnergiz4@gmail.com>
2026-07-16 22:48:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-
 const menuToggle = document.querySelector('.menu-toggle');
 const navMenu = document.querySelector('.nav-menu');
 const navLinks = document.querySelectorAll('.nav-link');
-
 
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active');
     navMenu.classList.toggle('active');
 });
-
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
